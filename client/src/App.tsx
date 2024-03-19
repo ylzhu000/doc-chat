48bbb6964a63a25681a047d79f020e2c464332e2
Yanlin(Eric) Zhu <ylinzhu19@gmail.com>
2024-03-18 21:27:33 -0400

    feature: setup basic routing for overall layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,20 @@
-import Button from '@mui/material/Button';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout';
+import NotFound from './pages/NotFound';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+
 export default function App() {
-  return <Button variant="contained">Hello world</Button>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/pages/NotFound.tsx
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>It seems like you got lost...</div>;
+}
