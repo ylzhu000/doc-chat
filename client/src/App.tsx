83959258ede4ed948cfaa64561c94738e6d4988b
Yanlin(Eric) Zhu <ylinzhu19@gmail.com>
2024-03-18 21:13:08 -0400

    feature: init react with vite client

---
+++ client/src/App.tsx
@@ -0,0 +1,4 @@
+import Button from '@mui/material/Button';
+export default function App() {
+  return <Button variant="contained">Hello world</Button>;
+}
---
+++ client/src/theme.tsx
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#67a0e3',
+      main: '#3b5998',
+      dark: '#2a4480',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ffbf80',
+      main: '#ff5722',
+      dark: '#c41c00',
+      contrastText: '#000',
+    },
+  },
+  typography: {
+    fontFamily: ['-apple-system', 'BlinkMacSystemFont', 'Poppins', 'sans-serif'].join(','),
+  },
+});
